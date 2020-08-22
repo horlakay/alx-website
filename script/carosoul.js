80cd3a36faa3cc1cfe2996852a57f9da159c1dd9
Hileamlak <hileamlak@gmail.com>
2020-08-22 20:46:22 +0300

    made the initiaal version

---
+++ script/carosoul.js
@@ -0,0 +1,14 @@
+let logoDiv = document.body.querySelector('.logo')
+let imagLink = [['html.svg','https://www.w3schools.com/html/default.asp'],['js.svg','https://www.w3schools.com/js/default.asp'],['css.svg','https://www.w3schools.com/css/default.asp']]
+
+let counter = 0
+setInterval(()=>{
+    if (counter>2){
+        counter=0
+    }
+   console.log(counter)
+    logoDiv.querySelector('a').href = imagLink[counter][1]
+    logoDiv.querySelector('.carosuole-image').src = `img/${imagLink[counter][0]}`
+    counter ++;
+
+},5000)
